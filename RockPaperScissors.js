e4a264462c5c7270da0479407bc36da41e852832
Daniel O Broin <danielobroin@gmail.com>
2022-01-01 22:38:18 +0000

    playRound function written and test play code temporarily added.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,14 +1,32 @@
 function computerPlay() {
 
-    let randomNumber
+    let randomNumber;
     randomNumber = Math.floor(Math.random() * 3);
 
-    let computerChoice = "Rock";
+    let computerChoice = "rock";
     if (randomNumber === 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     else  if (randomNumber === 2) {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "It's a tie! We both chose " + playerSelection + "."; 
+    }
+
+    if (playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "rock"){
+        return "loss";
+    }
+    
+    if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper"){
+        return "Win";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));

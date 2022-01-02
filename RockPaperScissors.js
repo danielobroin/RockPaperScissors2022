db93fc8bfd160a3d60928994c9d27ef74ae51320
Daniel O Broin <danielobroin@gmail.com>
2022-01-02 00:07:55 +0000

    Added game function with prompt for player input, loop to play five games, and score counter. Also reused but modified capitalise function from previous version of this project.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -15,7 +15,7 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
-        return "It's a tie! We both chose " + playerSelection + "."; 
+        return "tie"
     }
 
     if (playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "rock"){
@@ -23,10 +23,46 @@ function playRound(playerSelection, computerSelection){
     }
     
     if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper"){
-        return "Win";
+        return "win";
     }
 }
+// The below is test code which has been commented out.
+//const playerSelection = "rock";
+//const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let ties = 0;
+    let wins = 0
+    let losses = 0
+    for (i = 0; i<5; i++){
+        let playerSelection = window.prompt("Paper, scissors, or rock?").toLowerCase();
+        let computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection);
+
+        if (result == "tie"){
+            alert("It's a tie! We both chose " + playerSelection + "."); 
+            ties++;
+        }
+        else if (result == "win"){
+            alert("You win! " + capitalize(playerSelection) + " beats " + computerSelection + ".");
+            wins++;
+        }
+        else if (result == "loss"){
+            alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`)
+            losses++;
+        }
+    }
+    alert(`${losses} losses, ${wins} wins, and ${ties} ties.`)
+}
+
+game();
+
+
+function capitalize(string){ //This function is here to capitalise playerSelection and ComputerSelection at the start of sentences.
+    let firstLetter = string.charAt(0);
+    let capitalisedFirstLetter = firstLetter.toUpperCase();
+    let subsequentLetters = string.slice(1);
+    let capitalizedString = capitalisedFirstLetter + subsequentLetters;
+    return capitalizedString;
+}

0e22328d53452a91c1fe597bc2a5cd9d3c495523
Daniel O Broin <danielobroin@gmail.com>
2022-02-20 17:43:18 +0000

    Rock Paper Scissors basic UI added

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -13,9 +13,29 @@ function computerPlay() {
     return computerChoice;
 }
 
+const message = document.createElement("p")
+const container = document.querySelector('#container');
+const count = document.createElement("p")
+container.appendChild(message);
+container.appendChild(count);
+const winMessage = document.createElement("p")
+const loseMessage = document.createElement("p")
+winMessage.textContent = "You win! the game!"
+winMessage.style.color ='green';
+loseMessage.textContent = "I win this time!";
+loseMessage.style.color='red';
+
+const buttons = document.querySelectorAll("button")
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+        game(button.id);
+    });
+});
+
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
-        return "tie"
+        return "tie" 
     }
 
     if (playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "rock" && computerSelection == "paper" 
@@ -25,41 +45,55 @@ function playRound(playerSelection, computerSelection){
     
     if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "scissors" 
             || playerSelection == "scissors" && computerSelection == "paper"){
-        return "win";
+         return "win";
     }
 }
-// The below is test code which has been commented out.
-//const playerSelection = "rock";
-//const computerSelection = computerPlay();
-//console.log(playRound(playerSelection, computerSelection));
 
-function game() {
-    let ties = 0;
-    let wins = 0
-    let losses = 0
-    for (i = 0; i<5; i++){
-        let playerSelection = window.prompt("Paper, scissors, or rock?").toLowerCase();
-        let computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection);
+let ties = 0;
+let wins = 0;
+let losses = 0;
 
-        if (result == "tie"){
-            alert("It's a tie! We both chose " + playerSelection + "."); 
-            ties++;
-        }
-        else if (result == "win"){
-            alert("You win! " + capitalize(playerSelection) + " beats " + computerSelection + ".");
-            wins++;
-        }
-        else if (result == "loss"){
-            alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`)
-            losses++;
-        }
+function game(playerChoice) {
+    if (container.contains(loseMessage)){
+        container.removeChild(loseMessage);
     }
-    alert(`${losses} losses, ${wins} wins, and ${ties} ties.`)
+
+    if (container.contains(winMessage)){
+        container.removeChild(winMessage);
+    }
+  
+    let computerChoice = computerPlay();
+    let result = playRound(playerChoice, computerChoice)
+
+    if (result == "tie"){
+        message.textContent = "It's a tie! We both chose " + playerChoice + "."; 
+        ties++;
+    }
+    else if (result == "win"){
+        message.textContent =  "You win! " + capitalize(playerChoice) + " beats " + computerChoice + ".";
+        wins++;
+    }
+    else if (result == "loss"){
+        message.textContent = `You lose! ${capitalize(computerChoice)} beats ${playerChoice}.`
+        losses++;
+    }
+
+    count.textContent = (`${losses} losses, ${wins} wins, and ${ties} ties.`)
+    
+    if(wins>=5){
+        container.appendChild(winMessage);
+        ties = 0;
+        wins = 0;
+        losses = 0;
+    }
+
+    if(losses>=5){
+        container.appendChild(loseMessage);
+         ties = 0;
+         wins = 0;
+         losses = 0;
+    }
 }
-
-game();
-
 
 function capitalize(string){ //This function is here to capitalise playerSelection and ComputerSelection at the start of sentences.
     let firstLetter = string.charAt(0);
@@ -67,4 +101,4 @@ function capitalize(string){ //This function is here to capitalise playerSelecti
     let subsequentLetters = string.slice(1);
     let capitalizedString = capitalisedFirstLetter + subsequentLetters;
     return capitalizedString;
-}
+} 
